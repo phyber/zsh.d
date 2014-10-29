e0d7f5fd3eabb27121ae702718dc1e26ffc7aee9
phyber <phyber@localhost>
2014-10-29 15:20:59 +0000

    Changes to how ZSH syntax highlight install/update functions work.

--- zsh-syntax-highlighting.zsh
+++ zsh-syntax-highlighting.zsh
@@ -1,6 +1,11 @@
 # Syntax highlighting
-if [[ -f "${HOME}/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
-	source "${HOME}/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+ZSH_SYNTAX_HIGHLIGHT_DIR="${HOME}/.zshsh"
+
+if [[ -x "${ZSH_SYNTAX_HIGHLIGHT_DIR}" && \
+	-f "${ZSH_SYNTAX_HIGHLIGHT_DIR}/zsh-syntax-highlighting.zsh" ]]; then
+
+	source "${ZSH_SYNTAX_HIGHLIGHT_DIR}/zsh-syntax-highlighting.zsh"
+
 	# Can't modify this in .zshenv since we haven't loaded syntax
 	# highlighting at that time.
 	if [[ -n $ZSH_HIGHLIGHT_STYLES ]]; then
@@ -10,40 +15,52 @@ if [[ -f "${HOME}/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
 
 	# Function for simple, quick updating of the highlighting stuff.
 	function update-zsh-syntax-highlighting {
-		if (( $+commands[git] )); then
-			print "Updating ZSH Syntax Highlighting"
-			(
-				cd "${HOME}/.zsh-syntax-highlighting/"
-				git pull --quiet
-				return $?
-			)
-			if (( $? )); then
-				print -u 2 "Couldn't update ZSH Syntax Highlighting"
-				return 1
-			fi
-			exec "${SHELL}"
-		else
-			print -u 2 "git not installed. Cannot update ZSH Syntax Highlighting."
+		if (( ! $+commands[git] )); then
+			print -u 2 "git missing. Cannot update ZSH Syntax Highlighting."
+			return 1
 		fi
+
+		print "Updating ZSH Syntax Highlighting"
+		(
+			cd "${ZSH_SYNTAX_HIGHLIGHT_DIR}"
+			git pull --quiet
+			# Subshell, so we can return $? from here
+			# and be useful.
+			return $?
+		)
+		if (( $? )); then
+			print -u 2 "Couldn't update ZSH Syntax Highlighting"
+			return 1
+		fi
+		exec "${SHELL}"
 	}
 else
 	# If the syntax highlighting wasn't installed, create a function that
 	# will install it.
 	function install-zsh-syntax-highlighting {
-		if (( $+commands[git] )); then
-			print "Installing ZSH Syntax Highlighting"
-			git clone --quiet \
-				https://github.com/zsh-users/zsh-syntax-highlighting.git \
-				"${HOME}/.zsh-syntax-highlighting"
-			if (( $? )); then
-				print -u 2 "Couldn't install ZSH Syntax Highlighting"
-				return 1
-			fi
-			print "ZSH Syntax Highlighting installed."
-			exec "${SHELL}"
-		else
-			print -u 2 "git not installed. Cannot automatically install ZSH Syntax Highlighting."
-			print -u 2 "Download it from https://github.com/zsh-users/zsh-syntax-highlighting"
+		local -r ZSHSH_URL="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+
+		if [[ -z "${ZSH_SYNTAX_HIGHLIGHT_DIR}" ]]; then
+			print -u 2 "Error: ZSH_SYNTAX_HIGHLIGHT_DIR not set."
+			return 1
 		fi
+
+		if (( ! $+commands[git] )); then
+			print -u 2 "git missing. Cannot install ZSH Syntax Highlighting."
+			print -u 2 "Download it from ${ZSHSH_URL}"
+		fi
+
+		print "Installing ZSH Syntax Highlighting"
+
+		git clone --quiet \
+			"${ZSHSH_URL}" \
+			"${ZSH_SYNTAX_HIGHLIGHT_DIR}"
+		if (( $? )); then
+			print -u 2 "Couldn't install ZSH Syntax Highlighting"
+			return 1
+		fi
+
+		print "ZSH Syntax Highlighting installed."
+		exec "${SHELL}"
 	}
 fi
