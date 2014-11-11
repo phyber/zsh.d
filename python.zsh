42a2a06d16580282fce17f40aa02705acc8ad90c
phyber <phyber@localhost>
2014-11-11 10:31:24 +0000

    python.zsh: Make pip behave nicely.

--- python.zsh
+++ python.zsh
@@ -10,3 +10,15 @@ function _pip_completion {
 		PIP_AUTO_COMPLETE=1 $words[1] ) )
 }
 compctl -K _pip_completion pip
+
+# This makes pip install in usermode outside of virtualenvs but doesn't touch
+# the options inside a virtualenv.
+function pip {
+	if [[ -n "${VIRTUAL_ENV}" ]]; then
+		# If inside a virtual env, execute pip normally.
+		command pip $@
+	else
+		# Install things to user location by default.
+		PIP_USER=1 command pip $@
+	fi
+}
