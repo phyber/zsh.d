babb14cb9299243e8f867e6568f0364d991f1f7c
phyber <phyber@localhost>
2014-11-20 02:29:10 +0000

    man.zsh: Change command check. Removes indent level.

--- man.zsh
+++ man.zsh
@@ -1,14 +1,17 @@
-if (( $+commands[man] )); then
-	# Coloured man pages.
-	function man {
-		emulate -L zsh
-		LESS_TERMCAP_mb=$fg_bold[red] \
-		LESS_TERMCAP_md=$fg_bold[red] \
-		LESS_TERMCAP_me=$reset_color \
-		LESS_TERMCAP_se=$reset_color \
-		LESS_TERMCAP_so=$bg[blue]$fg_bold[yellow] \
-		LESS_TERMCAP_ue=$reset_color \
-		LESS_TERMCAP_us=$fg_bold[green] \
-		command man "$@"
-	}
+# Coloured man pages
+
+if (( ! $+commands[man] )); then
+	return
 fi
+
+function man {
+	emulate -L zsh
+	LESS_TERMCAP_mb=$fg_bold[red] \
+	LESS_TERMCAP_md=$fg_bold[red] \
+	LESS_TERMCAP_me=$reset_color \
+	LESS_TERMCAP_se=$reset_color \
+	LESS_TERMCAP_so=$bg[blue]$fg_bold[yellow] \
+	LESS_TERMCAP_ue=$reset_color \
+	LESS_TERMCAP_us=$fg_bold[green] \
+	command man "$@"
+}
