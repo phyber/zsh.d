164ea17756ffce3d6619cc9a4063be16afa388e3
phyber <phyber@localhost>
2014-11-20 02:27:41 +0000

    keychain.zsh: Change command check. Removes indent level.

--- keychain.zsh
+++ keychain.zsh
@@ -1,23 +1,26 @@
 # Load SSH keychain with all keys found in ~/.ssh if keychain command exists.
-if (( $+commands[keychain] )); then
-	function {
-		local -r keychain_file="${HOME}/.keychain/${HOST}-sh"
-		local timeout=$KEYCHAIN_TIMEOUT_MINS
-		if [[ -z "$timeout" ]]; then
-			timeout=()	# 0 = cache forever.
-		else
-			timeout=("--timeout" "$timeout")
-		fi
-		readonly timeout
 
-		keychain --quiet \
-			--nogui \
-			$timeout \
-			--host "${HOST}" \
-			${HOME}/.ssh/*.pub(:r)
-
-		if [[ -f ${keychain_file} ]]; then
-			source ${keychain_file}
-		fi
-	}
+if (( ! $+commands[keychain] )); then
+	return
 fi
+
+function {
+	local -r keychain_file="${HOME}/.keychain/${HOST}-sh"
+	local timeout=$KEYCHAIN_TIMEOUT_MINS
+	if [[ -z "$timeout" ]]; then
+		timeout=()	# 0 = cache forever.
+	else
+		timeout=("--timeout" "$timeout")
+	fi
+	readonly timeout
+
+	keychain --quiet \
+		--nogui \
+		$timeout \
+		--host "${HOST}" \
+		${HOME}/.ssh/*.pub(:r)
+
+	if [[ -f ${keychain_file} ]]; then
+		source ${keychain_file}
+	fi
+}
