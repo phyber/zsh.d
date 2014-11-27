e3d8e4eea274f9107b732e6c34b3d09bdef9b869
phyber <phyber@localhost>
2014-11-27 02:12:59 +0000

    go.zsh: Don't do some things if there is no go command

--- go.zsh
+++ go.zsh
@@ -1,10 +1,12 @@
-# GOPATH set to a hidden directory.
-export GOPATH="${HOME}/.gocode"
-
-# Add the $GOPATH bin directory to PATH
-export PATH="${GOPATH}/bin:${PATH}"
-
 # If we installed a local version of go, add that to PATH.
 if [[ -x "${HOME}/.local/go/bin" ]]; then
 	export PATH="${HOME}/.local/go/bin:${PATH}"
+fi
+
+if (( $+commands[go] )); then
+	# GOPATH set to a hidden directory.
+	export GOPATH="${HOME}/.gocode"
+
+	# Add the $GOPATH bin directory to PATH
+	export PATH="${GOPATH}/bin:${PATH}"
 fi
