54cf5e090e308594f8bd8704b79cba8364719666
phyber <phyber@localhost>
2015-01-12 14:27:56 +0000

    keychain.zsh: No need for anonymous function.

--- keychain.zsh
+++ keychain.zsh
@@ -4,23 +4,22 @@ if (( ! $+commands[keychain] )); then
 	return
 fi
 
-function {
-	local -r keychain_file="${HOME}/.keychain/${HOST}-sh"
-	local timeout=$KEYCHAIN_TIMEOUT_MINS
-	if [[ -z "$timeout" ]]; then
-		timeout=()	# 0 = cache forever.
-	else
-		timeout=("--timeout" "$timeout")
-	fi
-	readonly timeout
+local -r keychain_file="${HOME}/.keychain/${HOST}-sh"
+local timeout=$KEYCHAIN_TIMEOUT_MINS
 
-	keychain --quiet \
-		--nogui \
-		$timeout \
-		--host "${HOST}" \
-		${HOME}/.ssh/*.pub(:r)
+if [[ -z "$timeout" ]]; then
+	timeout=()	# cache forever.
+else
+	timeout=("--timeout" "$timeout")
+fi
+readonly timeout
 
-	if [[ -f ${keychain_file} ]]; then
-		source ${keychain_file}
-	fi
-}
+keychain --quiet \
+	--nogui \
+	$timeout \
+	--host "${HOST}" \
+	${HOME}/.ssh/*.pub(:r)
+
+if [[ -f ${keychain_file} ]]; then
+	source ${keychain_file}
+fi
