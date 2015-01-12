601ea5e9a96608afbc931fcf7bc63433f6fedadf
phyber <phyber@localhost>
2015-01-12 22:00:18 +0000

    ccache.zsh: Set CCACHE_PATH

--- ccache.zsh
+++ ccache.zsh
@@ -4,12 +4,16 @@ if (( ! $+commands[ccache] )); then
 	return
 fi
 
+# Should be the same on most systems
+export CCACHE_PATH="/usr/bin:/usr/local/bin"
+
+# ccache libexec location changes.
 case "$OSTYPE" in
 	linux*)
-		PATH="/usr/lib/ccache:$PATH"
+		export PATH="/usr/lib/ccache:$PATH"
 		;;
 	freebsd*)
-		PATH="/usr/local/libexec/ccache:$PATH"
+		export PATH="/usr/local/libexec/ccache:$PATH"
 		;;
 	*)
 		;;
