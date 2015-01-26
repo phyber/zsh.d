1276048c3d5143c35df8b83a90956b118c3f34e8
phyber <phyber@localhost>
2015-01-26 14:51:14 +0000

    ccache.zsh: Export CCACHE_DIR if running as root.

--- ccache.zsh
+++ ccache.zsh
@@ -14,3 +14,11 @@ case "$OSTYPE" in
 	*)
 		;;
 esac
+
+# If we're root, store ccache in /var.
+if _zshd_is_root; then
+	export CCACHE_DIR="/var/cache/ccache"
+	if [[ ! -d "${CCACHE_DIR}" ]]; then
+		mkdir -m 0700 "${CCACHE_DIR}"
+	fi
+fi
