94de6ffa576cc97db639a0d93e0dc29186bb7eda
phyber <phyber@localhost>
2015-04-07 10:07:29 +0100

    Add ruby gems paths to PATH.

--- ruby.zsh
+++ ruby.zsh
@@ -4,3 +4,10 @@ if (( $+commands[rbenv] )); then
 	export PATH="${HOME}/.rbenv/bin:${PATH}"
 	eval "$(rbenv init -)"
 fi
+
+# Add local ruby gems paths to PATH
+# TODO: Only do this for the active version of ruby.
+local p
+for p in ${HOME}/.gem/ruby/*; do
+	export PATH="$p/bin:$PATH"
+done
