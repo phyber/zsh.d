2e53cf8c62c28dea4f9b330483a70794b29bdb99
phyber <phyber@localhost>
2015-04-24 10:24:03 +0100

    Added vagrant.zsh

---
+++ functions/vagrant.zsh
@@ -0,0 +1,57 @@
+# vim:ft=zsh:
+
+# TODO: Autocompletion.
+
+function vacu {
+	vagrant cucumber $@
+	return $?
+}
+
+# Forces destruction and then ups clean VMs.
+function vadu {
+	vakill $@ && \
+	vaup $@
+	return $?
+}
+
+# Toggles VAGRANT_CUCUMBER_FORCE_COLOUR
+function vafc {
+	if [ $VAGRANT_CUCUMBER_FORCE_COLOUR ]; then
+		echo "Disabling forced vagrant cucumber colours."
+		unset VAGRANT_CUCUMBER_FORCE_COLOUR
+	else
+		echo "Enabling forced vagrant cucumber colours."
+		export VAGRANT_CUCUMBER_FORCE_COLOUR="on"
+	fi
+}
+
+function vaha {
+	vagrant halt $@
+	return $?
+}
+
+function vakill {
+	vagrant destroy -f $@
+	return $?
+}
+
+function varb {
+	vagrant snap rollback $@
+	return $?
+}
+
+# vast executes global-status, and removes the footer by exiting at the
+# first blank line it sees.
+function vast {
+	while read line; do
+		if [ ${#line} -eq 0 ]; then
+			break
+		fi
+		echo $line
+	done < <(vagrant global-status $@)
+	return $?
+}
+
+function vaup {
+	vagrant up --parallel $@
+}
