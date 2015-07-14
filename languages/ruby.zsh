2fc43113c21f09bb9618888794122c3a4a7a6970
phyber <phyber@localhost>
2015-07-15 00:01:52 +0100

    Try to ensure rbenv gems appear first in PATH

--- languages/ruby.zsh
+++ languages/ruby.zsh
@@ -1,10 +1,11 @@
 # Ruby related stuff.
 
+# Add system ruby gems paths to PATH
+# TODO: Only do this for the active version of ruby.
+_zshd_prepend_path ${HOME}/.gem/ruby/*/bin
+
+# Add rbenv gems last so they appear in the PATH first.
 if (( $+commands[rbenv] )); then
 	_zshd_prepend_path "${HOME}/.rbenv/bin"
 	eval "$(rbenv init -)"
 fi
-
-# Add local ruby gems paths to PATH
-# TODO: Only do this for the active version of ruby.
-_zshd_prepend_path ${HOME}/.gem/ruby/*/bin
