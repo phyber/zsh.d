265cc5a0ad87b900d24ca08d5be7a7364449c9e5
phyber <phyber@localhost>
2015-07-24 15:32:23 +0100

    git.zsh: Some protections to make sure dir is a git repo

--- functions/git.zsh
+++ functions/git.zsh
@@ -16,6 +16,17 @@ function _zshd_git_delete_remote_branch {
 function gitrmb {
 	local -r remote="$1"
 	local -r branch_name="$2"
+
+	if [ -z "${remote}" -o -z "${branch_name}" ]; then
+		print -u 2 "Usage: $0 REMOTE BRANCH"
+		return 1
+	fi
+
+	if ! git rev-parse --git-dir >/dev/null 2>&1; then
+		print -u 2 "Current directory is not a git repository."
+		return 1
+	fi
+
 	_zshd_git_delete_local_branch "${branch_name}"
 	_zshd_git_delete_remote_branch "${remote}" "${branch_name}"
 }
