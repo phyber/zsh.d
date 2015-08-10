7257704f2740de71b6b5db852a681778ec9b0fe9
phyber <phyber@localhost>
2015-08-10 15:38:30 +0100

    git.zsh: Minor tidy

--- functions/git.zsh
+++ functions/git.zsh
@@ -2,14 +2,12 @@
 function _zshd_git_delete_local_branch {
 	local -r branch_name="$1"
 	git branch -d "${branch_name}"
-	return $?
 }
 
 function _zshd_git_delete_remote_branch {
 	local -r remote="$1"
 	local -r branch_name="$2"
 	git push "${remote}" :"${branch_name}"
-	return $?
 }
 
 # gitrmb: git remove branch
