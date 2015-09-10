04ac20b7efa8cef5eeb48fa22db2588fe6e65667
phyber <phyber@localhost>
2015-09-10 22:09:19 +0100

    ruby.zsh: Auto setup shims and rehash PATH after gem install.

--- languages/ruby.zsh
+++ languages/ruby.zsh
@@ -2,10 +2,21 @@
 
 # Add system ruby gems paths to PATH
 # TODO: Only do this for the active version of ruby.
-_zshd_prepend_path ${HOME}/.gem/ruby/*/bin
+#_zshd_prepend_path ${HOME}/.gem/ruby/*/bin
 
 # Add rbenv gems last so they appear in the PATH first.
 if (( $+commands[rbenv] )); then
 	_zshd_prepend_path "${HOME}/.rbenv/bin"
 	eval "$(rbenv init -)"
 fi
+
+# Hijack the gem command to setup rbenv shims and rehash PATH after install.
+function gem {
+	command gem $@
+	if [ "$1" == 'install' ]; then
+		if (( $+commands[rbenv] )); then
+			rbenv rehash
+			hash -r
+		fi
+	fi
+}
