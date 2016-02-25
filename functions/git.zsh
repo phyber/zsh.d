969d47f8c158461964347187fb3e61bfa216d3da
phyber <phyber@localhost>
2016-02-25 15:03:09 +0000

    git.zsh: Add git cdup function

--- functions/git.zsh
+++ functions/git.zsh
@@ -75,3 +75,13 @@ function gitrmb {
 	_zshd_git_delete_local_branch "${branch_name}"
 	_zshd_git_delete_remote_branch "${remote}" "${branch_name}"
 }
+
+# gitcdup: Change directory to the top of the current git repo.
+function gitcdup {
+	local cdup=''
+	if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+		# If we're at the top level, this will become `cd ""` which
+		# will not change the current directory.
+		cd "$(git rev-parse --show-cdup)"
+	fi
+}
