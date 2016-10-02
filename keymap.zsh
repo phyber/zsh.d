498139ce697ea17361ca04c14cc54e5d3928f486
phyber <phyber@localhost>
2016-10-02 21:25:12 +0100

    Fix home/end keymap on FreeBSD.

--- keymap.zsh
+++ keymap.zsh
@@ -14,6 +14,12 @@ case "$OSTYPE" in
 		bindkey '^[[1~' beginning-of-line
 		bindkey '^[[4~' end-of-line
 		;;
+	freebsd*)
+		bindkey '^[[H' beginning-of-line
+		bindkey '^[[F' end-of-line
+		bindkey '^[[1~' beginning-of-line
+		bindkey '^[[4~' end-of-line
+		;;
 	*)
 		;;
 esac
