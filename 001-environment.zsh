50a5583f12d287ded3a680ec77e4482f2565a5c5
phyber <phyber@localhost>
2018-09-14 11:54:47 +0100

    Move common BSD type stuff into a function
    This is because the shell fall-through doesn't work quite as expected,
    although it's obvious in retrospect

--- 001-environment.zsh
+++ 001-environment.zsh
@@ -3,36 +3,42 @@
 HISTIGNORE='man:run-help'
 typeset -gT HISTIGNORE histignore # Tied array for easily checking HISTIGNORE
 
+# Common BSD type environment, used on BSD and OS X.
+function common_bsd_environment {
+    export CLICOLOR=1
+    export LSCOLORS="ExGxFxdxCxDxDxhbadExEx"
+    export TOP="-a -j"
+    export LS_OPTIONS='-F'
+
+    # Fix help.
+    unalias run-help
+    autoload run-help
+}
+
 # Handle some Linux/BSD differences here.
 # # ;& in the case enables fallthrough.
 case "$OSTYPE" in
-	linux*)
-		LS_OPTIONS='-F --group-directories-first'
-		;;
-	freebsd*|darwin*)
-		export CLICOLOR=1
-		export LSCOLORS="ExGxFxdxCxDxDxhbadExEx"
-		export TOP="-a -j"
-		LS_OPTIONS='-F'
+    linux*)
+        LS_OPTIONS='-F --group-directories-first'
+        ;;
+    freebsd*)
+        common_bsd_environment
 
-		# Fix help.
-		unalias run-help
-		autoload run-help
-		;&
-	freebsd*)
-		export RUBY_CONFIGURE_OPTS=--with-readline-dir=/usr/local
-		;;
-	darwin*)
-		# Executes a local install of ZSH if there is one, otherwise
-		# continues to use system zsh
-		_zshd_exec_usr_local_bin_zsh
-		;;
-	*)
-		;;
-esac
+        export RUBY_CONFIGURE_OPTS=--with-readline-dir=/usr/local
+        ;;
+    darwin*)
+        common_bsd_environment
+
+        # Executes a local install of ZSH if there is one, otherwise
+        # continues to use system zsh
+        _zshd_exec_usr_local_bin_zsh
+        ;;
+    *)
+        ;;
+    esac
 
 if (( $+commands[rlwrap] )); then
-	RLWRAP_BINS=(bc)
+    RLWRAP_BINS=(bc)
 fi
 
 DREPORT_DIR="${HOME}/Docs/Reports"
@@ -48,15 +54,15 @@ ZSHD_RBENV_DIR="${HOME}/.rbenv"
 
 # rbenv plugins
 ZSHD_RBENV_PLUGINS=(
-	rbenv/ruby-build
-	rbenv/rbenv-default-gems
-	rbenv/rbenv-gem-rehash
-	rkh/rbenv-update
+    rbenv/ruby-build
+    rbenv/rbenv-default-gems
+    rbenv/rbenv-gem-rehash
+    rkh/rbenv-update
 )
 
 # Sometimes we might not have vim installed. Redirect error output.
 if (( $+commands[vim] )); then
-	export EDITOR==vim
+    export EDITOR==vim
 fi
 export LC_COLLATE='C'
 export LESS='-R'
@@ -66,12 +72,12 @@ export WHOIS_OPTIONS='-H'
 
 # Enable lesspipe if it is present.
 if (( $+commands[lesspipe] )); then
-	eval $(lesspipe)
+    eval $(lesspipe)
 fi
 if (( $+commands[lesspipe.sh] )); then
-	eval $(lesspipe.sh)
+    eval $(lesspipe.sh)
 fi
 
 if (( $+commands[manpath] )); then
-	export MANPATH=$(manpath 2>/dev/null)
+    export MANPATH=$(manpath 2>/dev/null)
 fi
