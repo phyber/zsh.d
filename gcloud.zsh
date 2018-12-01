f0b65fb3c90660070d9a7a081795d90bdabf6690
phyber <phyber@localhost>
2018-12-01 19:08:55 +0000

    gcloud: comments

--- gcloud.zsh
+++ gcloud.zsh
@@ -1,8 +1,10 @@
-# Attempt to source gcloud completion from multiple places.
+# List of possible places gcloud lives.
 local gcloud_paths=(
     '/snap/google-cloud-sdk/current'
 )
 
+# Attempt to source gcloud completion from multiple places, loading the first
+# encountered and then breaking.
 local p
 for p in $gcloud_paths; do
     local inc_file="${p}/completion.zsh.inc"
