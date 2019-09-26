6b3024bc19700545662cd49bc958729cf5348761
phyber <phyber@localhost>
2019-09-26 15:08:24 +0100

    languages/go: Handle gvm if it's installed

--- languages/go.zsh
+++ languages/go.zsh
@@ -4,3 +4,8 @@ export GOPATH="${HOME}/.gocode"
 # If we installed a local version of go, add that to PATH.
 # And add the $GOPATH bin directory to PATH
 _zshd_prepend_path "${HOME}/.local/go/bin" "${GOPATH}/bin"
+
+# GVM handling
+if [ -f "${HOME}/.gvm/scripts/gvm" ]; then
+    source "${HOME}/.gvm/scripts/gvm"
+fi
