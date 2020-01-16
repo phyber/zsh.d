a595029597189c99bfb053383f4273d4a74e1688
phyber <phyber@localhost>
2020-01-16 16:15:03 +0000

    rust: Use sccache rustc wrapper if available

--- languages/rust.zsh
+++ languages/rust.zsh
@@ -3,3 +3,8 @@
 # Unconditionally add this to PATH, anticipating that we're always going to
 # install cargo.
 export PATH="${HOME}/.cargo/bin:${PATH}"
+
+# If sccache is installed, use as a Rust wrapper.
+if _zshd_check_cmd "sccache" 2>/dev/null; then
+    export RUSTC_WRAPPER="sccache"
+fi
