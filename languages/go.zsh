d2fcf3ebffcbd7b167ac3eb10993ac8934776799
phyber <phyber@localhost>
2023-02-09 16:19:27 +0000

    Disable GOTELEMETRY

--- languages/go.zsh
+++ languages/go.zsh
@@ -1,6 +1,9 @@
 # GOPATH set to a hidden directory.
 export GOPATH="${HOME}/.gocode"
 
+# Disable the upcoming Go telemtry stuff
+export GOTELEMETRY="off"
+
 # If we installed a local version of go, add that to PATH.
 # And add the $GOPATH bin directory to PATH
 _zshd_prepend_path "${HOME}/.local/go/bin" "${GOPATH}/bin"
