77b65d87048d9758bde8270bf05e9a0627fd6169
phyber <phyber@localhost>
2023-03-28 11:10:24 +0100

    rust: Add env var to enable sparse index

--- languages/rust.zsh
+++ languages/rust.zsh
@@ -4,6 +4,9 @@
 # install cargo.
 export PATH="${HOME}/.cargo/bin:${PATH}"
 
+# Use the sparse registry index
+export CARGO_REGISTRIES_CRATES_IO_PROTOCOL="sparse"
+
 # If sccache is installed, use as a Rust wrapper.
 if _zshd_check_cmd "sccache" 2>/dev/null; then
     export RUSTC_WRAPPER="sccache"
