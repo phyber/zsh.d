6b867ec57e2f03a7d83cef1f8b9695e7eec1d0a2
phyber <phyber@localhost>
2023-06-24 00:46:01 +0100

    go: disable automatic toolchain update

--- languages/go.zsh
+++ languages/go.zsh
@@ -4,6 +4,9 @@ export GOPATH="${HOME}/.gocode"
 # Disable the upcoming Go telemtry stuff
 export GOTELEMETRY="off"
 
+# Disable automatic toolchain downloading
+export GOTOOLCHAIN="path"
+
 # If we installed a local version of go, add that to PATH.
 # And add the $GOPATH bin directory to PATH
 _zshd_prepend_path "${HOME}/.local/go/bin" "${GOPATH}/bin"
